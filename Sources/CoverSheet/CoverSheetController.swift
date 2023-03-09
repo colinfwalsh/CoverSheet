2f68ec95f20c9f88b9598185a4c016a89abd5f1d
Colin Walsh <colinfwalsh@gmail.com>
2023-03-09 15:53:33 -0500

    .

--- Sources/CoverSheet/CoverSheetController.swift
+++ Sources/CoverSheet/CoverSheetController.swift
@@ -13,7 +13,7 @@ import Combine
 open class CoverSheetController: UIViewController, UIGestureRecognizerDelegate {
     
     @Published
-    private var currentState: SheetState = .custom(0.0)
+    private var currentState: SheetState = .custom("", 0.0)
     
     private var states: [SheetState] = []
     
